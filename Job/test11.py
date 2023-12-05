7684ffd115b2a3d5ddbc55e6976e1845ed1ead8a
JuliaDevil <821064526@qq.com>
2023-12-05 21:39:18 +0800

    test11

---
+++ Job/test11.py
@@ -0,0 +1,7 @@
+# -*- coding:utf-8 -*-
+import time
+
+if __name__ == '__main__':
+    # 1、获取当前时间，这样便于下面的使用。
+    now = time.strftime("%Y-%m-%d-%H_%M_%S", time.localtime(time.time()))
+    print(now)
